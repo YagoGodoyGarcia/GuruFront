7bcfb82daf48e650eb4b4891ee4f3ed43a1d9bae
YagoGodoyGarcia <yagogodoygarcia@outlook.com>
2025-05-31 20:53:11 -0300

    ms Clarity desativado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,17 +20,18 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/image.png" type="image/x-icon" sizes="7x10" />
+        {/**
         <Script
           id="clarity-script"
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `(function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                t=l.createElement(r);t.async=1;t.src="https://www.idprojeto.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "otaljzsf50");`,
           }}
-        />
+        /> */}
       </head>
       <body
         className={classNames([
